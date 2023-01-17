e5b64f2c03a37b312134696473c70fc392f234e5
huyn29 <99779691+huyn29@users.noreply.github.com>
2023-01-17 13:21:55 +0700

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 sudo yum update
-sudo yum install java-1.8.0 -y
+sudo amazon-linux-extras install java-openjdk11 -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
 https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
